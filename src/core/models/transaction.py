b01d4bd0ebe691ab299b6ac3b9b9e54a9ec701da
onetells <onetells@ya.ru>
2025-06-12 23:32:17 +0500

    New Version

--- src/core/models/transaction.py
+++ src/core/models/transaction.py
@@ -14,16 +14,16 @@ from core.models.user import User
 class Transaction(Base):
     __tablename__ = "transactions"
 
-    id: Mapped[uuid.UUID] = column(UUID(as_uuid=False), primary_key=True, server_default=text("uuid_generate_v4()"))
+    id: Mapped[uuid.UUID] = column(UUID(as_uuid=True), primary_key=True, server_default=text("uuid_generate_v4()"))
 
     ticker: Mapped[str] = column(Text, ForeignKey(Instrument.ticker, ondelete="CASCADE"), nullable=False)
     amount: Mapped[int] = column(Integer, nullable=False)
     price: Mapped[int] = column(Integer, nullable=False)
 
-    buyer_user_id: Mapped[uuid.UUID] = column(UUID(as_uuid=False), ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
-    seller_user_id: Mapped[uuid.UUID] = column(UUID(as_uuid=False), ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
+    buyer_user_id: Mapped[uuid.UUID] = column(UUID(as_uuid=True), ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
+    seller_user_id: Mapped[uuid.UUID] = column(UUID(as_uuid=True), ForeignKey(User.id, ondelete="CASCADE"), nullable=False)
 
-    buyer_order_id: Mapped[uuid.UUID] = column(UUID(as_uuid=False), ForeignKey(Order.id, ondelete="CASCADE"), nullable=False)
-    seller_order_id: Mapped[uuid.UUID] = column(UUID(as_uuid=False), ForeignKey(Order.id, ondelete="CASCADE"), nullable=False)
+    buyer_order_id: Mapped[uuid.UUID] = column(UUID(as_uuid=True), ForeignKey(Order.id, ondelete="CASCADE"), nullable=False)
+    seller_order_id: Mapped[uuid.UUID] = column(UUID(as_uuid=True), ForeignKey(Order.id, ondelete="CASCADE"), nullable=False)
 
     timestamp: Mapped[datetime] = column(TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
--- src/modules/users/schemes.py
+++ src/modules/users/schemes.py
@@ -1,12 +1,10 @@
-import uuid
-
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 
 from core.schemes.user import UserRole
 
 
 class UserModel(BaseModel):
-    id: uuid.UUID
+    id: UUID4
     name: str
     role: UserRole
     api_key: str
