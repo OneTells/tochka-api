5dc0b3f0cbd972848be017a0bdabaa145e6f5e5e
onetells <onetells@ya.ru>
2025-06-12 23:19:26 +0500

    New Version

--- src/modules/users/schemes.py
+++ src/modules/users/schemes.py
@@ -1,10 +1,12 @@
+import uuid
+
 from pydantic import BaseModel
 
 from core.schemes.user import UserRole
 
 
 class UserModel(BaseModel):
-    id: str
+    id: uuid.UUID
     name: str
     role: UserRole
     api_key: str
