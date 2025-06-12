34d5fa3b7f61f189c3a86c42c5f2155d0e9ffd4a
onetells <onetells@ya.ru>
2025-06-13 02:53:35 +0500

    New Version

--- src/modules/orders/api.py
+++ src/modules/orders/api.py
@@ -92,12 +92,12 @@ async def get_user_orders(
 
 @router.get("/order/{order_id}")
 async def get_order(
-    order: Annotated[UUID4, Path()],
+    order_id: Annotated[UUID4, Path()],
     user_id: Annotated[UserModel, Depends(Authentication(user_role=UserRole.USER))]
 ):
     response = await (
         Select(Order)
-        .where(Order.user_id == user_id, Order.status == OrderStatus.NEW, Order.id == order.id)
+        .where(Order.user_id == user_id, Order.status == OrderStatus.NEW, Order.id == order_id)
         .fetch_one(database)
     )
 
