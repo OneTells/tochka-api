c0f8fde3b12f89f5451c7541bba87a06e229d815
onetells <onetells@ya.ru>
2025-06-12 22:27:58 +0500

    New Version

--- src/core/schemes/settings.py
+++ src/core/schemes/settings.py
@@ -2,6 +2,8 @@ from everbase import DatabaseSettings
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
+print(open('.env').read())
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env', extra='ignore', env_nested_delimiter='__')
 
